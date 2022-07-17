e4dbb6dd063792becf7a3d4f09fb96f17bd097ef
Michael Wester <westermich@myvuw.ac.nz>
2022-07-17 23:36:22 +1200

    removed keyvault

--- bot.py
+++ bot.py
@@ -3,22 +3,11 @@ import discord
 from emulator import *
 from dotenv import load_dotenv
 import constants as c
-from azure.keyvault.secrets import SecretClient
-from azure.identity import DefaultAzureCredential
 
-
-credential = DefaultAzureCredential()
-
-secret_client = SecretClient(vault_url="https://discordkey.vault.azure.net/", credential=credential)
-
-secret = secret_client.get_secret("discordkey")
 
 def run():
     
     load_dotenv()
-    #TOKEN = os.getenv('DISCORD_TOKEN')
-    #print(os.environ['DISCORD_TOKEN'])
-    #TOKEN = os.environ['DISCORD_TOKEN']
     TOKEN = "OTU3MTMzNTk3NTc0MzIwMTk5.Yj6WIA.0o76rG4R93TGu7xxVa_pS2C3OWc"
     
     client = discord.Client()
@@ -40,13 +29,6 @@ def run():
         if message.content == '!keycheck':
             await message.channel.send('Token = ' + os.environ['DISCORD_TOKEN'])
             await message.channel.send('Token get = ' + os.environ.get('DISCORD_TOKEN'))
-            await message.channel.send(secret.value)
-            return
-        if message.content == '!meme2':
-            await message.channel.send('Memeing2...')
-            return
-        if message.content == '!meme3':
-            await message.channel.send('Memeing3...')
             return
         if message.content == '!a':
             a_button()
