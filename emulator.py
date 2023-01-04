7b6c43f9d895c9d1feaada4539e1a106d32a085c
Michael Wester <westermich@myvuw.ac.nz>
2023-01-05 03:05:58 +1300

    Changed pyboy log level to error. Moved image manipulation to new file. Movement commands return the image.

--- emulator.py
+++ emulator.py
@@ -1,13 +1,12 @@
 from pyboy import PyBoy
 from pyboy import WindowEvent
-from PIL import Image
+from pyboy import logger
 import constants as c
 from server_properties_editor import *
+logger.log_level("ERROR")
 
 
-def double_size(img):
-    new_img = img.resize((img.size[0]*2, img.size[1]*2), Image.ANTIALIAS)
-    return new_img
+
 
 def movement(press, release, server_id):
     server_id = str(server_id)
@@ -23,33 +22,34 @@ def movement(press, release, server_id):
     for i in range(60):
         pyboy.tick()
     #pyboy.screen_image().save('ss.png')
-    new_img = double_size(pyboy.screen_image())
-    #new_img = pyboy.screen_image()
-    new_img.save(server_id + '/' + c.screenshot_name)
+    # new_img = double_size(pyboy.screen_image())
+    # #new_img = pyboy.screen_image()
+    # new_img.save(server_id + '/' + c.screenshot_name)
     pyboy.tick()
     pyboy.save_state(open(server_id + '/' + game_type + '.gb.state', 'wb'))
     pyboy.stop()
+    return pyboy.screen_image()
 
 def a_button(server_id):
-    movement(WindowEvent.PRESS_BUTTON_A, WindowEvent.RELEASE_BUTTON_A, server_id)
+    return movement(WindowEvent.PRESS_BUTTON_A, WindowEvent.RELEASE_BUTTON_A, server_id)
 
 def b_button(server_id):
-    movement(WindowEvent.PRESS_BUTTON_B, WindowEvent.RELEASE_BUTTON_B, server_id)
+    return movement(WindowEvent.PRESS_BUTTON_B, WindowEvent.RELEASE_BUTTON_B, server_id)
 
 def up(server_id):
-    movement(WindowEvent.PRESS_ARROW_UP, WindowEvent.RELEASE_ARROW_UP, server_id)
+    return movement(WindowEvent.PRESS_ARROW_UP, WindowEvent.RELEASE_ARROW_UP, server_id)
 
 def down(server_id):
-    movement(WindowEvent.PRESS_ARROW_DOWN, WindowEvent.RELEASE_ARROW_DOWN, server_id)
+    return movement(WindowEvent.PRESS_ARROW_DOWN, WindowEvent.RELEASE_ARROW_DOWN, server_id)
 
 def left(server_id):
-    movement(WindowEvent.PRESS_ARROW_LEFT, WindowEvent.RELEASE_ARROW_LEFT, server_id)
+    return movement(WindowEvent.PRESS_ARROW_LEFT, WindowEvent.RELEASE_ARROW_LEFT, server_id)
 
 def right(server_id):
-    movement(WindowEvent.PRESS_ARROW_RIGHT, WindowEvent.RELEASE_ARROW_RIGHT, server_id)
+    return movement(WindowEvent.PRESS_ARROW_RIGHT, WindowEvent.RELEASE_ARROW_RIGHT, server_id)
 
 def start(server_id):
-    movement(WindowEvent.PRESS_BUTTON_START, WindowEvent.RELEASE_BUTTON_START, server_id)
+    return movement(WindowEvent.PRESS_BUTTON_START, WindowEvent.RELEASE_BUTTON_START, server_id)
 
 def select(server_id):
-    movement(WindowEvent.PRESS_BUTTON_SELECT, WindowEvent.RELEASE_BUTTON_SELECT, server_id)
+    return movement(WindowEvent.PRESS_BUTTON_SELECT, WindowEvent.RELEASE_BUTTON_SELECT, server_id)
