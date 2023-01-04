61365f03874bf72afbb2ca7030aec9d941375959
Michael Wester <westermich@myvuw.ac.nz>
2023-01-05 03:10:17 +1300

    added gif_name, prefixes, list of games

--- constants.py
+++ constants.py
@@ -1,2 +1,6 @@
 emulation_speed = 4
 screenshot_name = 'screenshot.png'
+gif_name = 'move.gif'
+cmd_prefix = "$"
+cmd_compound_prefix = "!!"
+list_of_games = ["!red", "!green", "!blue", "!yellow", "!silver", "!gold", "!crystal", "!coral"]
