1f271e7d8c93f47ea7bcd9f99c1d9adcef4e0746
Michael Wester <westermich@myvuw.ac.nz>
2023-01-09 18:51:36 +1300

    changed prefix to ! removed prefix from list_of_games added cmd_list

--- constants.py
+++ constants.py
@@ -1,6 +1,12 @@
+import dotenv
+
+dotenv.load_dotenv()
 emulation_speed = 4
 screenshot_name = 'screenshot.png'
 gif_name = 'move.gif'
-cmd_prefix = "$"
+cmd_prefix = "!"
 cmd_compound_prefix = "!!"
-list_of_games = ["!red", "!green", "!blue", "!yellow", "!silver", "!gold", "!crystal", "!coral"]
+list_of_games = ["red", "blue", "yellow", "silver", "gold", "crystal", "coral"]
+cmd_list = ["a", "b", "up", "down", "left", "right", "start", "select"]
+
+
