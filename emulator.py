b6018dc0a8b664d11757f9c3a62197462487ed96
Michael Wester <westermich@myvuw.ac.nz>
2023-01-09 18:52:37 +1300

    moved servers to server folder renamed move functions

--- emulator.py
+++ emulator.py
@@ -9,9 +9,9 @@ logger.log_level("ERROR")
 def movement(press, release, server_id):
     server_id = str(server_id)
     game_type = get_game_type(server_id)
-    pyboy = PyBoy(server_id + '/' + game_type + '.gb', window_type="headless")
-    if(open(server_id + '/' + game_type + '.gb.state', 'rb').read() != b''):
-        pyboy.load_state(open(server_id + '/' + game_type + '.gb.state', 'rb'))
+    pyboy = PyBoy("servers/" + server_id + '/' + game_type + '.gb', window_type="headless")
+    if(open("servers/" + server_id + '/' + game_type + '.gb.state', 'rb').read() != b''):
+        pyboy.load_state(open("servers/" + server_id + '/' + game_type + '.gb.state', 'rb'))
     pyboy.tick()
     pyboy.send_input(press)
     pyboy.tick()
@@ -20,7 +20,7 @@ def movement(press, release, server_id):
     for i in range(60):
         pyboy.tick()
     pyboy.tick()
-    pyboy.save_state(open(server_id + '/' + game_type + '.gb.state', 'wb'))
+    pyboy.save_state(open("servers/" + server_id + '/' + game_type + '.gb.state', 'wb'))
     pyboy.stop()
     return pyboy.screen_image()
 
@@ -30,20 +30,20 @@ def a_button(server_id):
 def b_button(server_id):
     return movement(WindowEvent.PRESS_BUTTON_B, WindowEvent.RELEASE_BUTTON_B, server_id)
 
-def up(server_id):
+def up_button(server_id):
     return movement(WindowEvent.PRESS_ARROW_UP, WindowEvent.RELEASE_ARROW_UP, server_id)
 
-def down(server_id):
+def down_button(server_id):
     return movement(WindowEvent.PRESS_ARROW_DOWN, WindowEvent.RELEASE_ARROW_DOWN, server_id)
 
-def left(server_id):
+def left_button(server_id):
     return movement(WindowEvent.PRESS_ARROW_LEFT, WindowEvent.RELEASE_ARROW_LEFT, server_id)
 
-def right(server_id):
+def right_button(server_id):
     return movement(WindowEvent.PRESS_ARROW_RIGHT, WindowEvent.RELEASE_ARROW_RIGHT, server_id)
 
-def start(server_id):
+def start_button(server_id):
     return movement(WindowEvent.PRESS_BUTTON_START, WindowEvent.RELEASE_BUTTON_START, server_id)
 
-def select(server_id):
+def select_button(server_id):
     return movement(WindowEvent.PRESS_BUTTON_SELECT, WindowEvent.RELEASE_BUTTON_SELECT, server_id)
