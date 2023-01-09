61e11c2f3afa2ea37980803f91182933b89ce166
Michael Wester <westermich@myvuw.ac.nz>
2023-01-09 18:53:39 +1300

    moved servers to servers folder images are now permanently saved in the images folder

--- image_helper.py
+++ image_helper.py
@@ -1,9 +1,23 @@
 from PIL import Image
-import constants as c
+import server_properties_editor as properties
 
-def double_size(img):
+
+def get_server_folder(server_id):
+    return "servers/" + str(server_id) + "/"
+
+def save_image(img, server_id):
+    
+    filepath = "servers/" + str(server_id) + "/" + "images/" + str(server_id) + "_" + str(properties.get_turn_count(server_id)) + ".png"
     new_img = img.resize((img.size[0]*2, img.size[1]*2), Image.ANTIALIAS)
+    new_img.save(filepath, "PNG")
+    return filepath
+
+def save_image_frame(img, server_id):
+    filepath = "servers/" + str(server_id) + "/" + "images/" + str(server_id) + "_" + str(properties.get_turn_count(server_id)) + ".png"
+    new_img = img.resize((img.size[0]*2, img.size[1]*2), Image.ANTIALIAS)
+    new_img.save(filepath, "PNG")
     return new_img
+
 
 def make_gif(images, filepath):   
     frame_one = images[0]
