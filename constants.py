fe83dbc45e8ce146598a6cccc26990b67bf08a32
Michael Wester <westermich@myvuw.ac.nz>
2023-01-12 01:20:49 +1300

    Added octagonal emoji id Added discord games image and gif urls Added bot invite url Added version

--- constants.py
+++ constants.py
@@ -4,9 +4,13 @@ dotenv.load_dotenv()
 emulation_speed = 4
 screenshot_name = 'screenshot.png'
 gif_name = 'move.gif'
-cmd_prefix = "!"
+cmd_prefix = "$"
 cmd_compound_prefix = "!!"
 list_of_games = ["red", "blue", "yellow", "silver", "gold", "crystal", "coral"]
 cmd_list = ["a", "b", "up", "down", "left", "right", "start", "select"]
-
+version = "0.1.0"
+bot_invite="https://discord.com/api/oauth2/authorize?client_id=957133597574320199&permissions=139586948160&scope=bot"
+games_image = "https://cdn.discordapp.com/attachments/957136740173365320/1061631724372643920/gameslist.png"
+games_gif = "https://cdn.discordapp.com/attachments/957136740173365320/1061555438124027974/games.gif"
+octagonal_sign = "<:octagonal_sign:1061655413151498340>"
 
