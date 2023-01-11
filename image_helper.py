ec64aa1171b0d74520d9db35f81f668c38b8bb53
Michael Wester <westermich@myvuw.ac.nz>
2023-01-12 01:31:34 +1300

    updated function names

--- image_helper.py
+++ image_helper.py
@@ -7,19 +7,19 @@ def get_server_folder(server_id):
 
 def save_image(img, server_id):
     
-    filepath = "servers/" + str(server_id) + "/" + "images/" + str(server_id) + "_" + str(properties.get_turn_count(server_id)) + ".png"
+    filepath = "servers/" + str(server_id) + "/" + "images/" + str(server_id) + "_" + str(properties.read_server_property_value(server_id, "turn_count")) + ".png"
     new_img = img.resize((img.size[0]*2, img.size[1]*2), Image.ANTIALIAS)
     new_img.save(filepath, "PNG")
     return filepath
 
 def save_image_frame(img, server_id):
-    filepath = "servers/" + str(server_id) + "/" + "images/" + str(server_id) + "_" + str(properties.get_turn_count(server_id)) + ".png"
+    filepath = "servers/" + str(server_id) + "/" + "images/" + str(server_id) + "_" + str(properties.read_server_property_value(server_id, "turn_count")) + ".png"
     new_img = img.resize((img.size[0]*2, img.size[1]*2), Image.ANTIALIAS)
     new_img.save(filepath, "PNG")
     return new_img
 
 
-def make_gif(images, filepath):   
+def make_gif(images, filepath, filename):   
     frame_one = images[0]
-    frame_one.save(filepath + "move.gif", format="GIF", append_images=images,
+    frame_one.save(filepath + filename, format="GIF", append_images=images,
                save_all=True, duration=1000, loop=1)    
