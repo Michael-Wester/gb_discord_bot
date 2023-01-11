15a05ffe7a6180997a5a8015606a76a2ecd8584a
Michael Wester <westermich@myvuw.ac.nz>
2023-01-12 01:15:16 +1300

    Added a number of config properties -date created -owner id -prefix -cmp_prefix -press_tick -release_tick -progress_bar -progress_bar_height -progress_bar_colour -cmd_set Made it easier to update and read property files

--- server_properties_editor.py
+++ server_properties_editor.py
@@ -16,10 +16,21 @@ def initialise_property_file(server_id, server_name, game_type):
         print("directory already exists")
 
     server_properties_file = open("servers/" + str(server_id) + "/" + str(server_id) + ".properties", "w")
+    server_properties_file.write("date_created=0")
+    server_properties_file.write("owner_id=0\n")
     server_properties_file.write("server_id=" + str(server_id) + "\n")
     server_properties_file.write("server_name=" + str(server_name) + "\n")
-    server_properties_file.write("game_type=" + game_type + "\n")
+    server_properties_file.write("game_type=" + game_type + "\n")   
     server_properties_file.write("turn_count=0\n")
+    server_properties_file.write("prefix=!\n")
+    server_properties_file.write("cmp_prefix=!!\n")
+    server_properties_file.write("press_tick=4\n")
+    server_properties_file.write("release_tick=60\n")
+    server_properties_file.write("progress_bar=0")
+    server_properties_file.write("progress_bar_height=0")
+    server_properties_file.write("progress_bar_colour=red")
+    server_properties_file.write("cmd_set=0")
+    
     server_properties_file.close()
 
 def get_game_type(server_id):
@@ -34,8 +45,7 @@ def copy_emulator_files(server_id, game_type):
     shutil.copyfile("roms/" + game_type + '/' + game_type +  ".gb.ram", "servers/" + str(server_id) + "/" + game_type + ".gb.ram")
     shutil.copyfile("roms/" + game_type + '/' + game_type +  ".gb.state", "servers/" + str(server_id) + "/" + game_type + ".gb.state")
 
-def server_exists(server_id):
-    
+def server_exists(server_id):  
     if os.path.exists(str(server_id)):
         return True
     else:
@@ -43,35 +53,32 @@ def server_exists(server_id):
 
 def delete_server_folder(server_id):
     shutil.rmtree("servers/" + str(server_id))
-    
 
-def increase_turn_count(server_id):
+def update_server_property_value(server_id, property, value):
     server_properties_file = open("servers/" + str(server_id) + "/" + str(server_id) + ".properties", "r")
     server_properties = server_properties_file.read().split('\n')
     server_properties_file.close()
-    turn_count = server_properties[3].split('=')[1]
-    if turn_count == '':
-        turn_count = 1
-    else:
-        turn_count = int(turn_count) + 1
     server_properties_file = open("servers/" + str(server_id) + "/" + str(server_id) + ".properties", "w")
-    server_properties_file.write(server_properties[0] + "\n")
-    server_properties_file.write(server_properties[1] + "\n")
-    server_properties_file.write(server_properties[2] + "\n")
-    server_properties_file.write("turn_count=" + str(turn_count) + "\n")
-    server_properties_file.close()
-    
-def get_turn_count(server_id):
+    for i in range(len(server_properties)):
+        if server_properties[i].split('=')[0] == property:
+            server_properties[i] = property + "=" + value
+        server_properties_file.write(server_properties[i] + "\n")
+
+def read_server_property_value(server_id, property):
     server_properties_file = open("servers/" + str(server_id) + "/" + str(server_id) + ".properties", "r")
     server_properties = server_properties_file.read().split('\n')
     server_properties_file.close()
-    turn_count = server_properties[3].split('=')[1]
-    if turn_count == '':
-        turn_count = 0
-    else:
-        turn_count = int(turn_count)
-    return turn_count
+    server_properties_file = open("servers/" + str(server_id) + "/" + str(server_id) + ".properties", "r")
+    for i in range(len(server_properties)):
+        if server_properties[i].split('=')[0] == property:
+            return server_properties[i].split('=')[1]
+    
+def increase_turn_count(server_id):
+    turn_count = read_server_property_value(server_id, "turn_count")
+    turn_count = int(turn_count) + 1
+    update_server_property_value(server_id, "turn_count", str(turn_count))
 
 def add_to_command_list(server_id, cmd, turn_count, author, time):
     with open("servers/" + str(server_id) + "/" + str(server_id) + ".data", "a") as f:
         f.write(cmd + "," + str(turn_count) + "," + author + "," + str(time) + "\n")
+
