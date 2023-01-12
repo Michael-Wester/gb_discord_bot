808c04cd9cedb4010c370a091d35f76711ce5ea5
Michael Wester <westermich@myvuw.ac.nz>
2023-01-12 15:13:09 +1300

    Added visual progress bar to gif Reformatted with python black

--- gif_maker.py
+++ gif_maker.py
@@ -8,11 +8,22 @@ def make_gif(folder, filepath):
         if file.endswith(".png"):
             img = Image.open(folder + file)
             images.append(img)
-            
+
     frame_one = images[0]
-    frame_one.save(filepath + "games.gif", format="GIF", append_images=images,
-               save_all=True, duration=1000, loop=0)    
+    frame_one_width = frame_one.size[0]
+    for i in range(len(images)):
+        progress_bar = Image.new(
+            "RGB", (int((i + 1) * frame_one_width / (len(images))), 10), "yellow"
+        )
+        images[i].paste(progress_bar, (0, 0))
+    frame_one.save(
+        filepath + "games.gif",
+        format="GIF",
+        append_images=images,
+        save_all=True,
+        duration=1000,
+        loop=0,
+    )
 
 
 make_gif("boxart/", "boxart/")
-           
--- image_helper.py
+++ image_helper.py
@@ -2,24 +2,57 @@ from PIL import Image
 import server_properties_editor as properties
 
 
-def get_server_folder(server_id):
-    return "servers/" + str(server_id) + "/"
-
 def save_image(img, server_id):
-    
-    filepath = "servers/" + str(server_id) + "/" + "images/" + str(server_id) + "_" + str(properties.read_server_property_value(server_id, "turn_count")) + ".png"
-    new_img = img.resize((img.size[0]*2, img.size[1]*2), Image.ANTIALIAS)
+    filepath = (
+        "servers/"
+        + str(server_id)
+        + "/"
+        + "images/"
+        + str(server_id)
+        + "_"
+        + str(properties.read_server_property_value(server_id, "turn_count"))
+        + ".png"
+    )
+    new_img = img.resize((img.size[0] * 2, img.size[1] * 2), Image.ANTIALIAS)
     new_img.save(filepath, "PNG")
     return filepath
 
+
 def save_image_frame(img, server_id):
-    filepath = "servers/" + str(server_id) + "/" + "images/" + str(server_id) + "_" + str(properties.read_server_property_value(server_id, "turn_count")) + ".png"
-    new_img = img.resize((img.size[0]*2, img.size[1]*2), Image.ANTIALIAS)
+    folderpath = "servers/" + str(server_id) + "/images/"
+    filepath = (
+        folderpath
+        + str(server_id)
+        + "_"
+        + str(properties.read_server_property_value(server_id, "turn_count"))
+        + ".png"
+    )
+    new_img = img.resize((img.size[0] * 2, img.size[1] * 2), Image.ANTIALIAS)
     new_img.save(filepath, "PNG")
     return new_img
 
 
-def make_gif(images, filepath, filename):   
+def make_gif(images, filepath, filename, server_id):
+    # images = images
+    bar_height = int(
+        properties.read_server_property_value(server_id, "progress_bar_height")
+    )
     frame_one = images[0]
-    frame_one.save(filepath + filename, format="GIF", append_images=images,
-               save_all=True, duration=1000, loop=1)    
+    frame_one_width = frame_one.size[0]
+    frame_one_height = frame_one.size[1]
+    if bar_height != 0:
+        for i in range(len(images)):
+            progress_bar = Image.new(
+                "RGB",
+                (int((i + 1) * frame_one_width / (len(images))), bar_height),
+                (255, 0, 0),
+            )
+            images[i].paste(progress_bar, (0, 0))
+    frame_one.save(
+        filepath + filename,
+        format="GIF",
+        append_images=images,
+        save_all=True,
+        duration=900,
+        loop=1,
+    )
