100bb4f9e5ab116764f5c7de5add774735c6cfdf
Michael Wester <westermich@myvuw.ac.nz>
2023-01-12 23:00:17 +1300

    Updated version

--- constants.py
+++ constants.py
@@ -8,7 +8,7 @@ cmd_prefix = "!"
 cmd_compound_prefix = "!!"
 list_of_games = ["red", "blue", "yellow", "silver", "gold", "crystal", "coral"]
 cmd_list = ["a", "b", "up", "down", "left", "right", "start", "select"]
-version = "0.1.0"
+version = "0.3.0"
 bot_invite = "https://discord.com/api/oauth2/authorize?client_id=957133597574320199&permissions=139586948160&scope=bot"
 games_image = "https://cdn.discordapp.com/attachments/957136740173365320/1061631724372643920/gameslist.png"
 games_gif = "https://cdn.discordapp.com/attachments/957136740173365320/1061555438124027974/games.gif"
