24f306aafb1b020baceaa8905e7f3ec02827c52f
Michael-Wester <michaeljrwester@gmail.com>
2023-02-06 13:41:55 +1300

    Added 2nd instance of pyboy for GBC mode.

--- pyboy_instance.py
+++ pyboy_instance.py
@@ -6,5 +6,7 @@ This is reused to reduce memory usage.
 Before I was creating a new pyboy instance each time a command was called.
 This was causing the bot to crash after a lot of commands.
 This can be any rom as it will be overwritten by loading the save state.
+Now there is two instances of pyboy, one for GB and one for GBC.
 """
-pyboy = PyBoy("roms/red/red.gb", window_type="headless")
+pyboy_gb = PyBoy("roms/red/red.gb", window_type="headless")
+pyboy_gbc = PyBoy("roms/gold/gold.gb", window_type="headless")
