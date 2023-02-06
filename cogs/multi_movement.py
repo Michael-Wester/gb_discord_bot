62c1033c5001926974e5bce71272bf2e4e73847a
Michael-Wester <michaeljrwester@gmail.com>
2023-02-06 17:14:00 +1300

    read prefix from file. now uses both pyboy instances

--- cogs/multi_movement.py
+++ cogs/multi_movement.py
@@ -7,7 +7,7 @@ import time
 import image_helper as h
 import constants as c
 import os
-from pyboy_instance import pyboy
+from pyboy_instance import pyboy_gb, pyboy_gbc
 import gc
 from emulator import Emulator
 
@@ -15,7 +15,8 @@ from emulator import Emulator
 class multi_movement(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.pyboy = pyboy 
+        self.pyboy_gb = pyboy_gb 
+        self.pyboy_gbc = pyboy_gbc
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -35,62 +36,75 @@ class multi_movement(commands.Cog):
         if os.path.exists(server_folder_path) == False:
             return
 
-        if cmd == c.cmd_compound_prefix:
+        try:
+            prefix = p.read_value(server_id, "cmp_prefix")
+        except:
+            print("Server properties not found.")        
+        
+        if str(message.content)[0:2] != prefix:
+            return
 
-            def get_cmd_list(server_id):
-                cmd_list_1 = ["w", "a", "s", "d", "e", "q", "r", "f"]
-                cmd_list_2 = ["2", "1", "3", "4", "3", "4", "5", "6"]
-                cmd_list_3 = ["2", "1", "3", "4", "a", "b", "z", "x"]
-                cmd_set = p.read_value(server_id, "cmd_set")
-                if cmd_set == "1":
-                    return cmd_list_1
-                if cmd_set == "2":
-                    return cmd_list_2
-                if cmd_set == "3":
+        def get_cmd_list(server_id):
+            cmd_list_1 = ["w", "a", "s", "d", "e", "q", "r", "f"]
+            cmd_list_2 = ["2", "1", "3", "4", "3", "4", "5", "6"]
+            cmd_list_3 = ["2", "1", "3", "4", "a", "b", "z", "x"]
+            cmd_set = p.read_value(server_id, "cmd_set")
+            if cmd_set == "1":
+                return cmd_list_1
+            if cmd_set == "2":
+                return cmd_list_2
+            if cmd_set == "3":
                     return cmd_list_3
             
             
-            def append_image(server_id, image_array, emulator, press, release):
-                emulator.movement(press, release)
-                image = emulator.save_screenshot()
-                image_array.append(h.save_image_frame(image, server_id))
-                p.record_cmd(server_id, char, message.author.name)
-                return image_array
+        def append_image(server_id, image_array, emulator, press, release):
+            emulator.movement(press, release)
+            image = emulator.save_screenshot()
+            image_array.append(h.save_image_frame(image, server_id))
+            p.record_cmd(server_id, char, message.author.name)
+            return image_array
 
-            emulator = Emulator(server_id, self.pyboy)
-            emulator.load_game()
+        if (p.read_value(server_id, "game_type") in ["red", "blue", "yellow"]):
+            self.pyboy = self.pyboy_gb
+        else:
+            self.pyboy = self.pyboy_gbc
             
-            images = []
-            cmd_list = get_cmd_list(server_id)
-            for char in cmd:
-                if char == cmd_list[4]:
-                    images = append_image(server_id, images, emulator, we.PRESS_BUTTON_A, we.RELEASE_BUTTON_A)
-                if char == cmd_list[5]:
-                    images = append_image(server_id, images, emulator, we.PRESS_BUTTON_B, we.RELEASE_BUTTON_B)                        
-                if char == cmd_list[0]:
-                    images = append_image(server_id, images, emulator, we.PRESS_ARROW_UP, we.RELEASE_ARROW_UP)
-                if char == cmd_list[1]:
-                    images = append_image(server_id, images, emulator, we.PRESS_ARROW_LEFT, we.RELEASE_ARROW_LEFT)
-                if char == cmd_list[2]:
-                    images = append_image(server_id, images, emulator, we.PRESS_ARROW_DOWN, we.RELEASE_ARROW_DOWN)
-                if char == cmd_list[3]:
-                    images = append_image(server_id, images, emulator, we.PRESS_ARROW_RIGHT, we.RELEASE_ARROW_RIGHT)
-                if char == cmd_list[6]:
-                    images = append_image(server_id, images, emulator, we.PRESS_BUTTON_SELECT, we.RELEASE_BUTTON_SELECT)
-                if char == cmd_list[7]:
-                    images = append_image(server_id, images, emulator, we.PRESS_BUTTON_START, we.RELEASE_BUTTON_START)
+            
+        emulator = Emulator(server_id, self.pyboy)
+        emulator.load_game()
+        
+        
+        images = []
+        cmd_list = get_cmd_list(server_id)
+        for char in cmd:
+            if char == cmd_list[4]:
+                images = append_image(server_id, images, emulator, we.PRESS_BUTTON_A, we.RELEASE_BUTTON_A)
+            if char == cmd_list[5]:
+                images = append_image(server_id, images, emulator, we.PRESS_BUTTON_B, we.RELEASE_BUTTON_B)                        
+            if char == cmd_list[0]:
+                images = append_image(server_id, images, emulator, we.PRESS_ARROW_UP, we.RELEASE_ARROW_UP)
+            if char == cmd_list[1]:
+                images = append_image(server_id, images, emulator, we.PRESS_ARROW_LEFT, we.RELEASE_ARROW_LEFT)
+            if char == cmd_list[2]:
+                images = append_image(server_id, images, emulator, we.PRESS_ARROW_DOWN, we.RELEASE_ARROW_DOWN)
+            if char == cmd_list[3]:
+                images = append_image(server_id, images, emulator, we.PRESS_ARROW_RIGHT, we.RELEASE_ARROW_RIGHT)
+            if char == cmd_list[6]:
+                images = append_image(server_id, images, emulator, we.PRESS_BUTTON_SELECT, we.RELEASE_BUTTON_SELECT)
+            if char == cmd_list[7]:
+                images = append_image(server_id, images, emulator, we.PRESS_BUTTON_START, we.RELEASE_BUTTON_START)
 
 
-            emulator.save()
-            h.make_gif(images, server_folder_path, "move.gif", server_id)
+        emulator.save()
+        h.make_gif(images, server_folder_path, "move.gif", server_id)
 
-            await message.channel.send(
-                file=discord.File(server_folder_path + c.gif_name)
-            )
-            time_end = time.time() - time_start
-            #await message.channel.send( "Time taken: " + str(time_end) + " seconds for an average of " + str(time_end / len(cmd)) + " seconds per command")
-            gc.collect
-            return
+        await message.channel.send(
+            file=discord.File(server_folder_path + c.gif_name)
+        )
+        time_end = time.time() - time_start
+        #await message.channel.send( "Time taken: " + str(time_end) + " seconds for an average of " + str(time_end / len(cmd)) + " seconds per command")
+        gc.collect
+        return
 
 
 async def setup(bot):
