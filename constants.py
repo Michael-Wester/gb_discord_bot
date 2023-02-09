f5703b64a5c6c078754109d56314ffa76f9d076b
Michael-Wester <michaeljrwester@gmail.com>
2023-02-10 02:13:28 +1300

    added arrow emoji constants

--- constants.py
+++ constants.py
@@ -8,3 +8,16 @@ bot_invite = "https://discord.com/api/oauth2/authorize?client_id=957133597574320
 games_image = "https://cdn.discordapp.com/attachments/957136740173365320/1061631724372643920/gameslist.png"
 games_gif = "https://cdn.discordapp.com/attachments/957136740173365320/1061555438124027974/games.gif"
 octagonal_sign = "<:octagonal_sign:1061655413151498340>"
+up_sign = "<:up_arrow:1061655413151498340>"
+down_sign = "<:down_sign:1073197027560796180>"
+left_sign = "<:left_arrow:1073196998594932786>"
+right_sign = "<:right_arrow:1073197066030944286>"
+a_sign = "<:regional_indicator_a:1073197760192450591>"
+b_sign = "<:regional_indicator_b:1073197791733633064>"
+
+
+
+# up_sign = ":arrow_up:"
+# down_sign = "\:arrow_down:"
+# left_sign = "\:arrow_left:"
+# right_sign = "\:arrow_right:"
