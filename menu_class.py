daec5dafb0bb9500ec89c82558406231cb8836ee
Michael-Wester <michaeljrwester@gmail.com>
2023-02-11 01:50:03 +1300

    Cleaned imports

--- menu_class.py
+++ menu_class.py
@@ -1,10 +1,4 @@
 import discord
-import constants as c
-from pyboy_instance import pyboy_gb, pyboy_gbc
-from emulator import Emulator
-import server_properties_editor as p
-import image_helper as h
-from pyboy import WindowEvent as we
 import button_helper as bh
 
 class Menu(discord.ui.View):
@@ -12,45 +6,43 @@ class Menu(discord.ui.View):
         super().__init__()
         discord.ui.View.timeout = None
         self.value = None
-        self.pyboy_gb = pyboy_gb 
-        self.pyboy_gbc = pyboy_gbc
         
     @discord.ui.button(label="LEFT", emoji="⬅️", style=discord.ButtonStyle.gray, row=0)
     async def left_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        embed, file = bh.press_button(self, interaction, cmd="left")
+        embed, file = bh.press_button(interaction, cmd="left")
         await interaction.response.edit_message(embed=embed, attachments=[file])
         
     @discord.ui.button(label="UP", emoji="⬆️", style=discord.ButtonStyle.gray, row=0)
     async def up_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        embed, file = bh.press_button(self, interaction, cmd="up")
+        embed, file = bh.press_button(interaction, cmd="up")
         await interaction.response.edit_message(embed=embed, attachments=[file])
 
     @discord.ui.button(label="DOWN", emoji="⬇️", style=discord.ButtonStyle.gray, row=0)
     async def down_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        embed, file = bh.press_button(self, interaction, cmd="down")
+        embed, file = bh.press_button(interaction, cmd="down")
         await interaction.response.edit_message(embed=embed, attachments=[file])
         
     @discord.ui.button(label="RIGHT", emoji="➡️", style=discord.ButtonStyle.gray, row=0)
     async def right_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        embed, file = bh.press_button(self, interaction, cmd="right")
+        embed, file = bh.press_button(interaction, cmd="right")
         await interaction.response.edit_message(embed=embed, attachments=[file])
         
     @discord.ui.button(label="A", emoji="🇦", style=discord.ButtonStyle.gray, row=1)
     async def a_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        embed, file = bh.press_button(self, interaction, cmd="a")
+        embed, file = bh.press_button(interaction, cmd="a")
         await interaction.response.edit_message(embed=embed, attachments=[file])
     
     @discord.ui.button(label="B", emoji="🇧", style=discord.ButtonStyle.gray, row=1)
     async def b_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        embed, file = bh.press_button(self, interaction, cmd="b")
+        embed, file = bh.press_button(interaction, cmd="b")
         await interaction.response.edit_message(embed=embed, attachments=[file])
     
     @discord.ui.button(label="START", emoji="🇸", style=discord.ButtonStyle.gray, row=1)
     async def start_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        embed, file = bh.press_button(self, interaction, cmd="start")
+        embed, file = bh.press_button(interaction, cmd="start")
         await interaction.response.edit_message(embed=embed, attachments=[file])
     
     @discord.ui.button(label="SELECT", emoji="🇸", style=discord.ButtonStyle.gray, row=1)
     async def select_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        embed, file = bh.press_button(self, interaction, cmd="select")
+        embed, file = bh.press_button(interaction, cmd="select")
         await interaction.response.edit_message(embed=embed, attachments=[file])
