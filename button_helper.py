e536959c1131fa9e2dc8a5cb0036d76bad941664
Michael-Wester <michaeljrwester@gmail.com>
2023-02-11 13:54:12 +1300

    moved cmd check to emualtor class.
    Display button presser in the embed

--- button_helper.py
+++ button_helper.py
@@ -15,24 +15,7 @@ def press_button(interaction, cmd):
         
     emulator = Emulator(server_id, pyboy)
     emulator.load_game()
-    
-    if cmd == "up":
-        emulator.movement(we.PRESS_ARROW_UP, we.RELEASE_ARROW_UP)
-    elif cmd == "down":
-        emulator.movement(we.PRESS_ARROW_DOWN, we.RELEASE_ARROW_DOWN)
-    elif cmd == "left":
-        emulator.movement(we.PRESS_ARROW_LEFT, we.RELEASE_ARROW_LEFT)
-    elif cmd == "right":
-        emulator.movement(we.PRESS_ARROW_RIGHT, we.RELEASE_ARROW_RIGHT)
-    elif cmd == "a":
-        emulator.movement(we.PRESS_BUTTON_A, we.RELEASE_BUTTON_A)
-    elif cmd == "b":
-        emulator.movement(we.PRESS_BUTTON_B, we.RELEASE_BUTTON_B)
-    elif cmd == "start":
-        emulator.movement(we.PRESS_BUTTON_START, we.RELEASE_BUTTON_START)
-    elif cmd == "select":
-        emulator.movement(we.PRESS_BUTTON_SELECT, we.RELEASE_BUTTON_SELECT)
-    
+    emulator.movement(cmd)
     
     img = emulator.save_screenshot()
     p.record_cmd(server_id, cmd, interaction.user.name)
@@ -41,6 +24,7 @@ def press_button(interaction, cmd):
     emulator.save()
     file = discord.File(img_filepath, filename="cmd.png")
     embed = discord.Embed()
+    embed.title = interaction.user.name + ": " + cmd
     embed.set_image(url="attachment://cmd.png")
     
     return embed, file
